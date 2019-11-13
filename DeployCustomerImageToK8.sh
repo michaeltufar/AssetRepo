3551b02b827f6bb80838bacc5f3699b6590af3af
michaeltufar <53825574+michaeltufar@users.noreply.github.com>
2019-11-13 15:28:41 +0100

    Update DeployCustomerImageToK8.sh
    Changed bearer token to MTUF

--- DeployCustomerImageToK8.sh
+++ DeployCustomerImageToK8.sh
@@ -2,5 +2,5 @@
 #
 
 cd /opt/rancher
-./rancher login https://k8s.eur.ad.sag/v3 --token token-vzcht:b6drnslrqsf2cnwnxr8jxp572zqkj4fl5hzxjq94tgl22qh4xdq2r9
+./rancher login https://k8s.eur.ad.sag/v3 --token token-rt5b7:wvjgn26pk7lqzwlwxbns6fwhmnhv67229rlmqvchl5pqpljkl7rccn
 ./rancher kubectl create -f ${WORKSPACE}/PushCustomerImageToK8.yaml 
